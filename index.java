91901a55478e98387526da1e24cecdaa7e56ce4f
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-28 20:46:50 -0300

    aula java

--- index.java
+++ index.java
@@ -1,16 +1,37 @@
 
-class main{
 
-    public static void main(String[] args){
+
+                    //Saida
+// class main{
+
+//     public static void main(String[] args){
 
         
 
-        System.out.println("Hello World");
+//         System.out.println("Hello World");
+//     }
+// }
+
+// // System.out.println()
+// // System.out.print()
+// // System.out.printf()
+
+
+// entrada
+
+import java.util.Scanner;
+class Main{
+
+    public static void main(String[] args){
+
+        System.out.println("Informe seu nome: ");
+        String palavra;
+        Scanner entrada = new Scanner(System.in);
+        palavra = entrada.next();
+
+        System.out.println("Seu nome é: " + palavra);
+        
+
+    
     }
 }
-
-// System.out.println()
-// System.out.print()
-// System.out.printf()
-
-
