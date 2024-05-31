3d10fad88872df84976af7656c16434fe8b38a4d
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-31 06:16:01 -0300

    condicionais

---
+++ condicionais.java
@@ -0,0 +1,14 @@
+ class Main {
+    
+
+    public static void main(String[] args){
+        int nota = 6;
+
+        if(nota >= 6){
+            System.out.println("Aprovado");
+        }else{
+            System.out.println("reprovado");
+        }
+    };
+    }
+
--- index.java
+++ index.java
@@ -1,82 +1,82 @@
 
 
 
-                    //Saida
-// class main{
+//                     //Saida
+// // class main{
 
-//     public static void main(String[] args){
+// //     public static void main(String[] args){
 
         
 
-//         System.out.println("Hello World");
-//     }
-// }
+// //         System.out.println("Hello World");
+// //     }
+// // }
 
-// // System.out.println()
-// // System.out.print()
-// // System.out.printf()
+// // // System.out.println()
+// // // System.out.print()
+// // // System.out.printf()
 
 
-// entrada
+// // entrada
 
-// import java.util.Scanner;
-// class Main{
+// // import java.util.Scanner;
+// // class Main{
 
-//     public static void main(String[] args){
+// //     public static void main(String[] args){
 
-//         System.out.println("Informe seu nome: ");
-//         String palavra;
-//         Scanner entrada = new Scanner(System.in);
-//         palavra = entrada.next();
+// //         System.out.println("Informe seu nome: ");
+// //         String palavra;
+// //         Scanner entrada = new Scanner(System.in);
+// //         palavra = entrada.next();
 
-//         System.out.println("Seu nome é: " + palavra);
+// //         System.out.println("Seu nome é: " + palavra);
         
 
     
+// //     }
+// // }
+
+// // Calculos
+// /* 
+// import java.util.Scanner;
+// class Main {
+//     public static void main(String[] args){
+//         double numA, numB, total;
+//         numA = 8;
+//         numB = 3;
+
+//         total = numA + numB;
+//         System.out.println(total);
+
+
 //     }
 // }
+// */
 
-// Calculos
-/* 
-import java.util.Scanner;
-class Main {
-    public static void main(String[] args){
-        double numA, numB, total;
-        numA = 8;
-        numB = 3;
+// // jogo
 
-        total = numA + numB;
-        System.out.println(total);
+// import java.util.Scanner;
+// import java.util.Random;
 
+// class Main {
+//     public static void main(String[] args) {
 
-    }
-}
-*/
+//         Random gerador = new Random();
+//         int x = gerador.nextInt(100);
 
-// jogo
+//         Scanner entrada = new Scanner(System.in);
+//         System.out.print("adivinhe o número: ");
 
-import java.util.Scanner;
-import java.util.Random;
-
-class Main {
-    public static void main(String[] args) {
-
-        Random gerador = new Random();
-        int x = gerador.nextInt(100);
-
-        Scanner entrada = new Scanner(System.in);
-        System.out.print("adivinhe o número: ");
-
-        int numero = entrada.nextInt();
+//         int numero = entrada.nextInt();
 
 
-        if(numero == x){
-            System.out.println("Pabéns você acertou: " + numero);
-        }else{
-            System.out.println("você errou, era o numero: " + x);
-        }
+//         if(numero == x){
+//             System.out.println("Pabéns você acertou: " + numero);
+//         }else{
+//             System.out.println("você errou, era o numero: " + x);
+//         }
         
-    }
+//     }
 
-}
+// }
 
