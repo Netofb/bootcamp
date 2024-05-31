3493b1ad2c05d674c208dcba9f568397dc913d7f
Fabio Barros lima <fabi.neto.fb@gmail.com>
2024-05-31 06:34:01 -0300

    switch

--- condicionais.java
+++ condicionais.java
@@ -2,12 +2,24 @@
     
 
     public static void main(String[] args){
-        int nota = 6;
+        int nota = 5;
 
-        if(nota >= 6){
-            System.out.println("Aprovado");
-        }else{
-            System.out.println("reprovado");
+        // if(nota >= 6){
+        //     System.out.println("Aprovado");
+        // }else{
+        //     System.out.println("reprovado");
+        // }
+
+
+        switch (nota) {
+            case 6:
+                System.out.println("passou");
+                break ;
+                case 5:
+                System.out.println(" não passou");
+                break ;
+            default:
+                break;
         }
     };
     }
